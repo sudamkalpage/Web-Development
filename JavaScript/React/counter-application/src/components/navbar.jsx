94a0a7699e63264c7c7353ad2dcde300e621fe9f
sudamkalpage <e16168@eng.pdn.ac.lk>
2022-03-04 09:37:13 +0530

    React-counterApp Feat(app): Stateless functional components

--- JavaScript/React/counter-application/src/components/navbar.jsx
+++ JavaScript/React/counter-application/src/components/navbar.jsx
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-          <span className="badge badge-pill badge-secondary m-2">
-            {this.props.countersNo}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-pill badge-secondary m-2">
+          {props.countersNo}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;

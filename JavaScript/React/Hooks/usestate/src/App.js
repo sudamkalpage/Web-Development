f46092e7344014759fefc95d6dde8f11a684b3b4
sudamkalpage <e16168@eng.pdn.ac.lk>
2022-03-13 13:18:44 +0530

    React:Material UI: Tables (Data Table, Dense Table, Collapse Table)

--- JavaScript/React/Hooks/usestate/src/App.js
+++ JavaScript/React/Hooks/usestate/src/App.js
@@ -5,19 +5,55 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [state, setState] = useState({ no: 0, color: "Yellow" });
+
+  function Increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function Decrement() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  function ChangeState() {
+    setState((prevState) => {
+      return { ...prevState, no: prevState.no - 1 };
+    });
+  }
 
   return (
     <React.Fragment>
       <Navbar countersNo={0} />
       <main className="container">
-        <button className="btn btn-primary  btn-lg m-2">+</button>
+        <button className="btn btn-primary  btn-lg m-2" onClick={Increment}>
+          +
+        </button>
         <span
           style={{ fontSize: 25 }}
           className="badge badge-pill badge-warning m-2 badge"
         >
-          2
+          {count}
         </span>
-        <button className="btn btn-danger btn-lg m-2">-</button>
+        <button className="btn btn-danger btn-lg m-2" onClick={Decrement}>
+          -
+        </button>
+        <br></br>
+
+        <span
+          style={{ fontSize: 25 }}
+          className="badge badge-pill badge-warning m-2 badge"
+        >
+          {state.color}
+        </span>
+        <span
+          style={{ fontSize: 25 }}
+          className="badge badge-pill badge-warning m-2 badge"
+        >
+          {state.no}
+        </span>
+        <button className="btn btn-danger btn-lg m-2" onClick={ChangeState}>
+          -
+        </button>
       </main>
     </React.Fragment>
   );

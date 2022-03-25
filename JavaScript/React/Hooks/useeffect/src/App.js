eedd3d31d43811a12e298129949763a66b8e77d8
sudamkalpage <e16168@eng.pdn.ac.lk>
2022-03-25 16:52:10 +0530

    React-UseEffect feat(badge): Added badge with all configurations

--- JavaScript/React/Hooks/useeffect/src/App.js
+++ JavaScript/React/Hooks/useeffect/src/App.js
@@ -9,6 +9,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import { borders } from "@mui/system";
 
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
@@ -30,13 +31,29 @@ function App() {
           </Button>
         </div>
         <div>
-          <Box component="div" sx={{ display: "inline", m: 2 }}>
-            <ThemeProvider theme={theme}>
+          <ThemeProvider theme={theme}>
+            <Box
+              component="div"
+              sx={{
+                m: 2,
+                width: "10%",
+                textAlign: "center",
+                color: "secondary.main",
+                bgcolor: "warning.main",
+                border: 1,
+                borderRadius: "8px",
+                borderColor: "primary.main",
+              }}
+            >
+              <Typography variant="h5">visible</Typography>
+            </Box>
+
+            <Box component="div" sx={{ display: "inline", m: 2 }}>
               <Typography variant="h3">Responsive h3</Typography>
               <Typography variant="h4">Responsive h4</Typography>
               <Typography variant="h5">Responsive h5</Typography>
-            </ThemeProvider>
-          </Box>
+            </Box>
+          </ThemeProvider>
         </div>
       </Container>
     </React.Fragment>

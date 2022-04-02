2f892d2709d34509f0d047184ba6453e4672c2c8
sudamkalpage <e16168@eng.pdn.ac.lk>
2022-04-02 16:34:53 +0530

    Ts-ReactDataGrid Init(app): Initial commit

---
+++ TypeScript/react-data-grid/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DataGrid from "./components/DataGrid";
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <DataGrid />
+    </div>
+  );
+}
+
+export default App;
